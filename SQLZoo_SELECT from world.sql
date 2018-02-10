a412093b708bb057aaccaa5fc97d7fb0ea5413c0
Boyu HA <Adienus.ha@gmail.com>
2018-02-10 18:24:53 -0500

    Update SQLZoo_SELECT from world.sql

--- SQLZoo_SELECT from world.sql
+++ SQLZoo_SELECT from world.sql
@@ -6,16 +6,16 @@
 
 
 
-1.
-Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+#1.
+#Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
 
 
 SELECT name, continent, population FROM world
 
 
 
-2.
-How to use WHERE to filter records. Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
+#2.
+#How to use WHERE to filter records. Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
 
 
 SELECT name FROM world
@@ -23,10 +23,8 @@ WHERE population > 200000000
 
 
 
-3.
-Give the name and the per capita GDP for those countries with a population of at least 200 million.
-
-HELP:How to calculate per capita GDP
+#3.
+#Give the name and the per capita GDP for those countries with a population of at least 200 million.
 
 
 select name,gdp/population as pc_gdp
@@ -35,8 +33,8 @@ where population > 200000000
 
 
 
-4.
-Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions.
+#4.
+#Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions.
 
 
 select name,population/1000000 as 'Population (M)' 
@@ -45,8 +43,8 @@ where continent = 'South America'
 
 
 
-5.
-Show the name and population for France, Germany, Italy
+#5.
+#Show the name and population for France, Germany, Italy
 
 
 select name, population
@@ -55,8 +53,8 @@ where name in ('France','Germany','Italy')
 
 
 
-6.
-Show the countries which have a name that includes the word 'United'
+#6.
+#Show the countries which have a name that includes the word 'United'
 
 
 select name
@@ -65,10 +63,10 @@ where name like 'United%'
 
 
 
-7.
-Two ways to be big: A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million.
+#7.
+#Two ways to be big: A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million.
 
-Show the countries that are big by area or big by population. Show name, population and area.
+#Show the countries that are big by area or big by population. Show name, population and area.
 
 
 select name,population ,area
@@ -77,13 +75,13 @@ where area > 3000000 or population > 250000000
 
 
 
-8.
-Exclusive OR (XOR). Show the countries that are big by area or big by population but not both. Show name, population and area.
+#8.
+#Exclusive OR (XOR). Show the countries that are big by area or big by population but not both. Show name, population and area.
 
-Australia has a big area but a small population, it should be included.
-Indonesia has a big population but a small area, it should be included.
-China has a big population and big area, it should be excluded.
-United Kingdom has a small population and a small area, it should be excluded.
+#Australia has a big area but a small population, it should be included.
+#Indonesia has a big population but a small area, it should be included.
+#China has a big population and big area, it should be excluded.
+#United Kingdom has a small population and a small area, it should be excluded.
 
 
 select name,population,area
@@ -92,11 +90,11 @@ where (area < 3000000 and population > 250000000) or (area > 3000000 and populat
 
 
 
-9.
-Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+#9.
+#Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
 
-For South America show population in millions and GDP in billions both to 2 decimal places.
-Millions and billions
+#For South America show population in millions and GDP in billions both to 2 decimal places.
+#Millions and billions
 
 
 select name,round(population/1000000,2) as 'Population(M)',round(gdp/1000000000,2) as 'GDP(B)'
@@ -105,10 +103,10 @@ where continent = 'South America'
 
 
 
-10.
-Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+#10.
+#Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
 
-Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+#Show per-capita GDP for the trillion dollar countries to the nearest $1000.
 
 
 select name,round(gdp/population/1000,0)*1000
@@ -117,14 +115,14 @@ where gdp > 1000000000000
 
 
 
-11.
-Greece has capital Athens.
+#11.
+#Greece has capital Athens.
 
-Each of the strings 'Greece', and 'Athens' has 6 characters.
+#Each of the strings 'Greece', and 'Athens' has 6 characters.
 
-Show the name and capital where the name and the capital have the same number of characters.
+#Show the name and capital where the name and the capital have the same number of characters.
 
-You can use the LENGTH function to find the number of characters in a string
+#You can use the LENGTH function to find the number of characters in a string
 
 
 SELECT name,  capital
@@ -133,12 +131,12 @@ where length(name) = length(capital)
 
 
 
-12.
-The capital of Sweden is Stockholm. Both words start with the letter 'S'.
+#12.
+#The capital of Sweden is Stockholm. Both words start with the letter 'S'.
 
-Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
-You can use the function LEFT to isolate the first character.
-You can use <> as the NOT EQUALS operator.
+#Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+#You can use the function LEFT to isolate the first character.
+#You can use <> as the NOT EQUALS operator.
 
 
 select name,capital
@@ -147,62 +145,15 @@ where left(capital,1) = left(name,1) and (capital != name)
 
 
 
-13.
-Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. They don't count because they have more than one word in the name.
+#13.
+#Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. They don't count because they have more than one word in the name.
 
-Find the country that has all the vowels and no spaces in its name.
+#Find the country that has all the vowels and no spaces in its name.
 
-You can use the phrase name NOT LIKE '%a%' to exclude characters from your results.
-The query shown misses countries like Bahamas and Belarus because they contain at least one 'a'
+#You can use the phrase name NOT LIKE '%a%' to exclude characters from your results.
+#The query shown misses countries like Bahamas and Belarus because they contain at least one 'a'
 
 
 select name
 from world
 where name not like '%a%e%i%o%u%' and name not like '% %'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
